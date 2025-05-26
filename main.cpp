71d8869d6baf0a6e061f9a64aaae35087d15b3bb
gg3z <54788437+gg3z@users.noreply.github.com>
2025-05-26 00:52:39 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include "lseg_intersector.h"
+#include <fstream>
+#include <iostream>
+
+/*
+ * The code in this folder finds all intersections for a given input list of
+ * 2d line segments. It has only been tested on a CMake build in VS Code, but
+ * should not present difficulties to build and run in other build environments
+ *
+ * Algorithmically, it is structured in 2 phases:
+ * - filtering of non overlap/intersection pairs of segments
+ *    - this is done by first sorting the x-coordinates of all segments
+ *    - while tracking the owning segment,
+ *    - then sweeping through the x-values and aggregating pairs of indices
+ *    - the resulting pairs are further filtered along other directions
+ *
+ * - fine computation of the intersection result for each remaining pair
+ *    - first compute the transverse intersection if determinant != 0
+ *    - if needed, calculate all min distances from each endpoint to the other
+ *      segment
+ *
+ * Computational complexity: for an input list of n segments, the cost of this
+ * algorithm will be o(nlog(n)) which is comparable to the Bentley-Ottman
+ * algorithm and is optimal.
+ * The algorithm as implemented is similar to the Bentley-Ottman approach and
+ * will run faster when there are fewer intersections relative to the total
+ * number of pairs.
+ *
+ * Possible enhancements to the current code:
+ * - performance: filtering can easily be improved by choosing additional
+ *  directions
+ * - performance: intersection code can be streamlined to run faster
+ *
+ * - functionality: intersection types can be distinguished between:
+ *                  - transverse intersection
+ *                  - touch intersection (with interior of one segment)
+ *                  - touch intersection (end-to-end)
+ *                  - overlap of length > tol
+ *   functionality: the above intersection types would allow end-to-end
+ *                  intersections in a polyline to be ignored
+ * - functionality: reporting intersections between 2 or more groups of segments
+ *                  would be highly desirable
+ *
+ * - testing: more test cases to be added for better case coverage
+ *            - for both pair-wise intersection and intersection detection
+ *              between groups of segments
+ *
+ * - architecture: abstract classes can be defined with interfaces that don't
+ *  depend on the implementation
+ */
+
+int main()
+{
+  // test_lineseg_intx();
+  cout << "--- running test case 1 -----------\n";
+  int n1 = test_intersector_1();
+  cout << "--- running test case 2 -----------\n";
+  int n2 = test_intersector_2();
+  cout << "--- running test case 3 -----------\n";
+
+  const int nSegments = 10000;
+  std::string fname = "data_10000.txt";
+  std::ofstream timing_out(fname);
+
+  // for testing and benchmarking purposes
+  for (double maxSegLen = 0.01; maxSegLen < 0.21; maxSegLen += 0.01) {
+    [[maybe_unused]] int n3 =
+        test_intersector_3(timing_out, nSegments, maxSegLen);
+  }
+  timing_out.close();
+  return 0;
+}
